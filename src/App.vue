3815c492172c8b13f5ec163749aba17cfbd4e7f0
albannikov <104481138+albannikov@users.noreply.github.com>
2023-01-27 15:40:04 +0500

    header

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,42 @@ export default {
 </script>
 
 <template>
-  <router-view></router-view>
-  dssdffd
+<!-- Header -->
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <img src="./img/logo.png" class="logo">
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 d-flex flex-row mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Главная</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Избранное</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">О проекте</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+    Кнопки
+      </div>
+    </header>
+  </div>
+ <!-- Header --> 
+  <router-view></router-view>  
+  Футер
 </template>
 
 <style scoped>
+
+
+header {
+width: 1170px;
+height: 112px;
+left: 132px;
+top: 38px;
+}
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+width: 112px;
+height: 112px;
+left: 132px;
+top: 38px;
 }
 </style>
