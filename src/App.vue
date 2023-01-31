e268b62a4efc6d727df8b5cbc27d48cfd857ae8c
albannikov <104481138+albannikov@users.noreply.github.com>
2023-01-31 08:54:29 +0500

    Главная

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,101 @@
 <script>
-// import Dashboard from './components/Dashboard.vue';
-// import Auth from './components/Auth.vue';
-export default {
-  
-}
+ import ModalWindow from './components/modal-reg.vue'
+ 
+ export default {
+     name: 'App',
+     components: {
+         ModalWindow
+     },
+     methods: {
+         showModal: function () {
+             this.$refs.modal.show = true
+         }
+     },
+ }
 </script>
 
 <template>
 <!-- Header -->
+<header class="font-header">
 <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <img src="./img/logo.png" class="logo">
-      </a>
-
-      <ul class="nav col-12 col-md-auto mb-2 d-flex flex-row mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Главная</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Избранное</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">О проекте</a></li>
-      </ul>
-
-      <div class="col-md-3 text-end">
-    Кнопки
-      </div>
-    </header>
+  <div class="row d-flex align-items-center">
+  <div class="col-6 col-md-4"> 
+    <a href="/"><img src="./img/logo.png" class="logo"></a>
   </div>
+  <div class="col-6 col-md-4">
+    <ul class="nav col col-xs-12">
+        <li><a href="#" class="nav-link px-2 link-secondary">Главная</a></li>
+        <!-- <li><a href="#" class="nav-link px-2 link-dark">Избранное</a></li> -->
+        <li><a href="#" class="nav-link px-2 link-dark">О проекте</a></li>
+      </ul>  
+  </div>
+  <div class="col-6 col-md-4 d-flex justify-content-end">
+    <button class="btn-reg show-modal-button" @click="showModal">Регистрация</button> 
+    <button class="btn-enter show-modal-button" @click="showModal">Вход</button> 
+  </div>
+  </div>
+</div>
+
+
+
+ 
+
+ <modal-window ref="modal"></modal-window>
+</header>
+
+  
  <!-- Header --> 
   <router-view></router-view>  
   Футер
 </template>
 
 <style scoped>
+.btn-reg {
+box-sizing: border-box;
+width: 188px;
+height: 49px;
+background: #FFFFFF;
+border: 1px solid #000000;
+border-radius: 5px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+color: #000000;
+margin-right: 30px;
+}
 
+.btn-enter {
+background: #000000;
+border-radius: 5px;
+width: 123.57px;
+height: 49px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+color: #FFFFFF;
+}
 
 header {
-width: 1170px;
 height: 112px;
-left: 132px;
-top: 38px;
+padding-top: 32px;
 }
 .logo {
 width: 112px;
 height: 112px;
 left: 132px;
 top: 38px;
+}
+
+.font-header {
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+color: #000000;
 }
 </style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,42 +1,80 @@
 <script>
- import ModalWindow from '../components/modal-reg.vue'
- 
- export default {
-     name: 'App',
-     components: {
-         ModalWindow
-     },
-     methods: {
-         showModal: function () {
-             this.$refs.modal.show = true
-         }
-     },
- }
+
 </script>
 
 <template>
   
- <h2>kjdshkjfhdskfhkjsdh</h2>
- <div class="container">
- <div class="row align-items-center">
-    <div class="col">
-      Одна из трёх колонок
+
+ <div class="container content">
+ <div class="row">
+    <div class="col-sm-4 xol-xs-12">
+      <div class="card">
+      <img class="img-card" src="../img/N4513.jpg">
+      <h1>Персонажи</h1>
+      <span class="description">Зайди и познакомься со всеми персонажами вселенной</span>      
+      </div>
     </div>
-    <div class="col">
-      Одна из трёх колонок
+    <div class="col-sm-4 xol-xs-12">
+      <div class="card">
+      <img class="img-card" src="../img/Anatomy_Park_7 1.jpg">
+      <h1>Локации</h1>
+      <span class="description">Исследуй все локации. Давай же, не будь занудой!</span>      
+      </div>
     </div>
-    <div class="col">
-      Одна из трёх колонок
+    <div class="col-sm-4 xol-xs-12">
+      <div class="card">
+      <img class="img-card" src="../img/rick-and-morty 1.jpg">
+      <h1>Эпизоды</h1>
+      <span class="description">Узнай чуть больше о карте приключений Рика и Морти</span>      
+      </div>
     </div>
   </div>
 </div>
 
- <button сlass="show-modal-button" @click="showModal">Показать модальное окно</button> 
- <modal-window ref="modal"></modal-window>
+
 
 
 </template>
 
 <style scoped>
+.content {
+padding-top: 90px;
+}
+.card {
+box-sizing: border-box;
+background: #FFFFFF;
+border: 1px dashed rgba(162, 162, 162, 0.6);
+border-radius: 19px;
+padding: 20px;
+/* margin: 10px; */
+}
 
+.card:hover {
+border: 1px rgba(0, 0, 0, 0.6);
+box-shadow: 6px 6px 7px 3px rgba(46, 46, 46, 0.58);
+border-radius: 19px;
+padding: 20px;
+margin: 10px;
+}
+
+.img-card {
+border-radius: 8px;
+}
+
+.description {
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 20px;
+color: #000000;
+}
+h1 {
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 36px;
+line-height: 44px;
+color: #000000;
+}
 </style>
